4ad0ead1004cbc4923bd829fdb06795bbbc544f6
sbalkrishna <sanbhujb@in.ibm.com>
2016-05-29 22:49:37 +0530

    Added static list of books in home page

--- app/src/main/java/com/example/android/books/BookDetails.java
+++ app/src/main/java/com/example/android/books/BookDetails.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -42,8 +43,35 @@ public class BookDetails extends AppCompatActivity {
         auther_name_textView.setText("Auther: "+intent.getStringExtra("auther_name"));
 
         book_cover_imageView = (ImageView) findViewById(R.id.book_details_cover);
-//        book_cover_imageView.setImageResource(intent.getIntExtra("book_cover",5));
-        Picasso.with(this).load(intent.getStringExtra("book_cover")).into(book_cover_imageView);
+        //book_cover_imageView.setImageResource(intent.getIntExtra("book_cover",5));
+        //Picasso.with(this).load(intent.getStringExtra("book_cover")).into(book_cover_imageView);
+        Log.e("book cover log",intent.getStringExtra("book_cover"));
+        if (intent.getStringExtra("book_cover").contains("http")) {
+            Log.e("temp","hellooooo");
+            Picasso.with(this).load(intent.getStringExtra("book_cover")).into(book_cover_imageView);
+        }
+        else
+        {
+            String img_url = intent.getStringExtra("book_cover");
+            if(img_url.contains("the_alchemist"))
+                Picasso.with(this).load(R.drawable.the_alchemist).into(book_cover_imageView);
+            else if(img_url.contains("out_of_box"))
+                Picasso.with(this).load(R.drawable.out_of_the_box).into(book_cover_imageView);
+            else if(img_url.contains("five_point"))
+                Picasso.with(this).load(R.drawable.five_point_someone).into(book_cover_imageView);
+            else if(img_url.contains("one_night"))
+                Picasso.with(this).load(R.drawable.one_night).into(book_cover_imageView);
+            else if(img_url.contains("revolution"))
+                Picasso.with(this).load(R.drawable.revolution2020).into(book_cover_imageView);
+            else if(img_url.contains("half_girlfriend"))
+                Picasso.with(this).load(R.drawable.half_girlfriend).into(book_cover_imageView);
+            else if(img_url.contains("two_states"))
+                Picasso.with(this).load(R.drawable.two_states).into(book_cover_imageView);
+            else if(img_url.contains("3_mistakes"))
+                Picasso.with(this).load(R.drawable.three_mistakes).into(book_cover_imageView);
+        }
+
+
 
         shortDescription = (TextView) findViewById(R.id.description);
         shortDescription.setText(intent.getStringExtra("short_description"));
@@ -80,7 +108,31 @@ public class BookDetails extends AppCompatActivity {
 
                 dialogue_cover_image = (ImageView)settingsDialog.findViewById(R.id.id_cover_dialogue_image);
 //                dialogue_cover_image.setImageResource(intent.getIntExtra("book_cover",0));
-                Picasso.with(v.getContext()).load(intent.getStringExtra("book_cover")).into(dialogue_cover_image);
+                //Picasso.with(v.getContext()).load(intent.getStringExtra("book_cover")).into(dialogue_cover_image);
+
+                if (intent.getStringExtra("book_cover").contains("http")) {
+                    Picasso.with(v.getContext()).load(intent.getStringExtra("book_cover")).into(dialogue_cover_image);
+                }
+                else
+                {
+                    String img_url = intent.getStringExtra("book_cover");
+                    if(img_url.contains("the_alchemist"))
+                        Picasso.with(v.getContext()).load(R.drawable.the_alchemist).into(dialogue_cover_image);
+                    else if(img_url.contains("out_of_box"))
+                        Picasso.with(v.getContext()).load(R.drawable.out_of_the_box).into(dialogue_cover_image);
+                    else if(img_url.contains("five_point"))
+                        Picasso.with(v.getContext()).load(R.drawable.five_point_someone).into(dialogue_cover_image);
+                    else if(img_url.contains("one_night"))
+                        Picasso.with(v.getContext()).load(R.drawable.one_night).into(dialogue_cover_image);
+                    else if(img_url.contains("revolution"))
+                        Picasso.with(v.getContext()).load(R.drawable.revolution2020).into(dialogue_cover_image);
+                    else if(img_url.contains("half_girlfriend"))
+                        Picasso.with(v.getContext()).load(R.drawable.half_girlfriend).into(dialogue_cover_image);
+                    else if(img_url.contains("two_states"))
+                        Picasso.with(v.getContext()).load(R.drawable.two_states).into(dialogue_cover_image);
+                    else if(img_url.contains("3_mistakes"))
+                        Picasso.with(v.getContext()).load(R.drawable.three_mistakes).into(dialogue_cover_image);
+                }
 
                 settingsDialog.show();
             }
--- app/src/main/java/com/example/android/books/MainActivityFragment.java
+++ app/src/main/java/com/example/android/books/MainActivityFragment.java
@@ -58,7 +58,6 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -101,26 +100,26 @@ public class MainActivityFragment extends Fragment {
 
     private void initializeData(String url) {
         books = new ArrayList<>();
-        if(url == "")
-        {
-            return;
-        }
-//commenting out the static book data
 
-//        books.add(new Book("The Alchemist", "Paulo Coelho", R.drawable.the_alchemist, R.drawable.the_alchemist, "The protagonist of The Alchemist is a young Andalusian shepherd who lives in Spain. He begins his journey to Egypt when he is chased by his frequent dreams of a mysterious child who coaxes him to look for treasure buried at the base of the Egyptian pyramids. He comes across Melchizedek, the king of Salem during his journey to Egypt. Santiago is enlightened by him to the concept of a 'Personal Legend' or a quest that would help him realize the core and depth of the desires buried in his heart. He meets an aspiring alchemist and a beautiful young woman en route. He also meets an alchemist who accompanies him in his expedition to enlighten him with his profound wisdom and knowledge. Through this journey he discovers the truths regarding why the treasure evades him each time. The Alchemist is a journey of wisdom, self-discovery and philosophy and finding your own 'Personal Legend' against the comparatively invaluable material possessions.", "image_url"));
-//        books.add(new Book("Out of the Box", "Harsha Bhogle", R.drawable.out_of_the_box, R.drawable.out_of_the_box, "India s Richie Benaud equivalent, the guru of cricket from India. Adam Gilchrist Aptly described as India s first non-playing cricket celebrity , Harsha Bhogle has captured the entire gamut of Indian cricket in his weekly column in the Indian Express: from the advent of Dhoni to the decline of the Fab Four; from the art of coaching the Indian team to the BCCI s money-mindedness; from cricketers shooting commercials just before a match to the nail-biting finish of many an IPL clash. Out of the Box brings together the very best of Harsha s writing, in a book that will be a veritable delight to any cricket fan. Knowledgeable, frank and witty, and with a sense of drama comparable to that of cricket itself, Harsha is a master at evoking the many moods of the game. And he is at his best when paying tribute to cricketing greats: Lara, Jayasuriya, Ganguly, Laxman, Dravid, Sehwag and the incomparable Tendulkar. As he follows India s fortunes on the cricket field at home and overseas, Harsha asks the question that is on everyone s lips today: can India really be no.1 in all three forms of the game? The answer lies in our history and in the pages of Out of the Box.", "img_url"));
-//        books.add(new Book("Five point someone", "Chetan Bhagat", R.drawable.five_point_someone, R.drawable.five_point_someone, "A Chetan Bhagats debut novel that presents a dark and humorous perspective on the indian education system. Set in the premises of IIT, the story revolves around three students who become friends while struggling to keep their place in this highly esteemed institute.", "img_url"));
-//        books.add(new Book("half girlfriend", "Chetan Bhagat", R.drawable.half_girlfriend, R.drawable.half_girlfriend, "Once upon a time there was a Bihari boy called Madhav. He fell in love with a girl called Riya. \nMadhav didn't speak English well. Riya did. \nMadhav wanted a relationship. Riya didn't. \nRiya just wanted friendship. Madhav didn't. \nRiya suggested a compramise. \nShe agreed to be his half girlfriend.", "img_url"));
-//        books.add(new Book("2 States", "Chetan Bhagat", R.drawable.two_states, R.drawable.two_states, "Love marriages around the world are simple:\nBoy loves girl. Girl loves boy. They get married. \nIn India, there are a few more steps: \nBoy loves girl. Girl loves boy. \nGirls's family has to love the boy. Boy's family has to love girl. \nGirls family has to love boy's family. Boy's family has to love girl's family. \nGirl and boy still love each other. They get married.\n\nWelcome to 2 States, the story of Krish and Ananya who are from two different states of India, deeply in love with each other and want to get married. Of course their parents don't agree. To convert their love story into love marriage, the couple has a tough battle ahead of them, for it is easy to fight and rebel, but much harder to convince. Will they make it?\n", "img_url"));
-//        books.add(new Book("The 3 mistakes of my life", "Chetan Bhagat", R.drawable.three_mistakes, R.drawable.three_mistakes, "In late 2000, a young boy in Ahmedabad dreams of owning a business. To accomodate his friends passion, he opens a cricket shop. However, nothing comes easy in a turbulent city. To realise their goals, they will have to face it all-religious, politics, calamities, unacceptable love and, above all, their own mistakes. Will they make it? Can an individual's dreams overcome the nightmares of real life? Can we succeed despite mistakes?", "img_url"));
-//        books.add(new Book("One night at the call center", "Chetan Bhagat", R.drawable.one_night, R.drawable.one_night, "In winter 2004, a writer met a young girl on an overnight train journey. To pass the time she offered to tell him a story. However she had one condition: that he make it into his second book. He hesitated but asked what the story is about. \n\nThe girl said the story was about six people working in a call center, set in one night. \n\nShe said it was the night they got a phone call. \n\nA phone call from God. \n\nAre you ready to take the call?", "img_url"));
-//        books.add(new Book("Revolution 2020", "Chetan Bhagat", R.drawable.revolution2020, R.drawable.revolution2020, "Once upon a time, in small-town India, thre lived two intelligent boys. \n\nOne wanted to use his intelligence to make money. \n\nOne wanted to use his intelligence to start a revolution. \n\nThe problem was they both loved the same girl. \n\nWelcome to Revolution 2020. A story about childhood friends Gopal, Raghav and Aarti, who struggle to find success, love and happiness in varanasi. However it is not easy to attain these in an unfair society that rewards the corrupt. As Gopal gives into the system, and Raghav fights it, who will win?", "img_url"));
-
-//        URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=chetanbhagat");
-//        String url = "https://www.googleapis.com/books/v1/volumes?q=harrypotter";
+        books.add(new Book("The Alchemist", "Paulo Coelho", R.drawable.the_alchemist, R.drawable.the_alchemist, "The protagonist of The Alchemist is a young Andalusian shepherd who lives in Spain. He begins his journey to Egypt when he is chased by his frequent dreams of a mysterious child who coaxes him to look for treasure buried at the base of the Egyptian pyramids. He comes across Melchizedek, the king of Salem during his journey to Egypt. Santiago is enlightened by him to the concept of a 'Personal Legend' or a quest that would help him realize the core and depth of the desires buried in his heart. He meets an aspiring alchemist and a beautiful young woman en route. He also meets an alchemist who accompanies him in his expedition to enlighten him with his profound wisdom and knowledge. Through this journey he discovers the truths regarding why the treasure evades him each time. The Alchemist is a journey of wisdom, self-discovery and philosophy and finding your own 'Personal Legend' against the comparatively invaluable material possessions.", "the_alchemist"));
+        books.add(new Book("Out of the Box", "Harsha Bhogle", R.drawable.out_of_the_box, R.drawable.out_of_the_box, "India s Richie Benaud equivalent, the guru of cricket from India. Adam Gilchrist Aptly described as India s first non-playing cricket celebrity , Harsha Bhogle has captured the entire gamut of Indian cricket in his weekly column in the Indian Express: from the advent of Dhoni to the decline of the Fab Four; from the art of coaching the Indian team to the BCCI s money-mindedness; from cricketers shooting commercials just before a match to the nail-biting finish of many an IPL clash. Out of the Box brings together the very best of Harsha s writing, in a book that will be a veritable delight to any cricket fan. Knowledgeable, frank and witty, and with a sense of drama comparable to that of cricket itself, Harsha is a master at evoking the many moods of the game. And he is at his best when paying tribute to cricketing greats: Lara, Jayasuriya, Ganguly, Laxman, Dravid, Sehwag and the incomparable Tendulkar. As he follows India s fortunes on the cricket field at home and overseas, Harsha asks the question that is on everyone s lips today: can India really be no.1 in all three forms of the game? The answer lies in our history and in the pages of Out of the Box.", "out_of_box"));
+        books.add(new Book("Five point someone", "Chetan Bhagat", R.drawable.five_point_someone, R.drawable.five_point_someone, "A Chetan Bhagats debut novel that presents a dark and humorous perspective on the indian education system. Set in the premises of IIT, the story revolves around three students who become friends while struggling to keep their place in this highly esteemed institute.", "five_point"));
+        books.add(new Book("half girlfriend", "Chetan Bhagat", R.drawable.half_girlfriend, R.drawable.half_girlfriend, "Once upon a time there was a Bihari boy called Madhav. He fell in love with a girl called Riya. \nMadhav didn't speak English well. Riya did. \nMadhav wanted a relationship. Riya didn't. \nRiya just wanted friendship. Madhav didn't. \nRiya suggested a compramise. \nShe agreed to be his half girlfriend.", "half_girlfriend"));
+        books.add(new Book("2 States", "Chetan Bhagat", R.drawable.two_states, R.drawable.two_states, "Love marriages around the world are simple:\nBoy loves girl. Girl loves boy. They get married. \nIn India, there are a few more steps: \nBoy loves girl. Girl loves boy. \nGirls's family has to love the boy. Boy's family has to love girl. \nGirls family has to love boy's family. Boy's family has to love girl's family. \nGirl and boy still love each other. They get married.\n\nWelcome to 2 States, the story of Krish and Ananya who are from two different states of India, deeply in love with each other and want to get married. Of course their parents don't agree. To convert their love story into love marriage, the couple has a tough battle ahead of them, for it is easy to fight and rebel, but much harder to convince. Will they make it?\n", "two_states"));
+        books.add(new Book("The 3 mistakes of my life", "Chetan Bhagat", R.drawable.three_mistakes, R.drawable.three_mistakes, "In late 2000, a young boy in Ahmedabad dreams of owning a business. To accomodate his friends passion, he opens a cricket shop. However, nothing comes easy in a turbulent city. To realise their goals, they will have to face it all-religious, politics, calamities, unacceptable love and, above all, their own mistakes. Will they make it? Can an individual's dreams overcome the nightmares of real life? Can we succeed despite mistakes?", "3_mistakes"));
+        books.add(new Book("One night at the call center", "Chetan Bhagat", R.drawable.one_night, R.drawable.one_night, "In winter 2004, a writer met a young girl on an overnight train journey. To pass the time she offered to tell him a story. However she had one condition: that he make it into his second book. He hesitated but asked what the story is about. \n\nThe girl said the story was about six people working in a call center, set in one night. \n\nShe said it was the night they got a phone call. \n\nA phone call from God. \n\nAre you ready to take the call?", "one_night"));
+        books.add(new Book("Revolution 2020", "Chetan Bhagat", R.drawable.revolution2020, R.drawable.revolution2020, "Once upon a time, in small-town India, thre lived two intelligent boys. \n\nOne wanted to use his intelligence to make money. \n\nOne wanted to use his intelligence to start a revolution. \n\nThe problem was they both loved the same girl. \n\nWelcome to Revolution 2020. A story about childhood friends Gopal, Raghav and Aarti, who struggle to find success, love and happiness in varanasi. However it is not easy to attain these in an unfair society that rewards the corrupt. As Gopal gives into the system, and Raghav fights it, who will win?", "revolution"));
 
         //Data will be fetched from the google books api
-        new fetchBooks().execute(url);
+
+        if(url != "") {
+            new fetchBooks().execute(url);
+        }
+        else
+        {
+            RVAdapter adapter = new RVAdapter(books);
+            rv.setAdapter(adapter);
+        }
     }
 
     public class fetchBooks extends AsyncTask<String, Void, Object> {
@@ -237,7 +236,6 @@ public class MainActivityFragment extends Fragment {
                         books.add(book);
                     } catch (Exception e) {
                         Log.e("Json Parsing error", e.toString());
-                        return "emptyBuffer";
                     }
                 }
 
@@ -304,7 +302,31 @@ public class MainActivityFragment extends Fragment {
             /*Pass the data to recycler view */
             holder.bookName.setText(books.get(position).book_name);
             holder.autherName.setText(books.get(position).book_auther);
-            Picasso.with(getContext()).load(books.get(position).getImg_url()).into(holder.bookCover);
+            Log.e("book cover url",books.get(position).getImg_url());
+            if (books.get(position).getImg_url().contains("http")) {
+                Picasso.with(getContext()).load(books.get(position).getImg_url()).into(holder.bookCover);
+            }
+            else
+            {
+                String img_url = books.get(position).getImg_url();
+                if(img_url == "the_alchemist")
+                    Picasso.with(getContext()).load(R.drawable.the_alchemist).into(holder.bookCover);
+                else if(img_url == "out_of_box")
+                    Picasso.with(getContext()).load(R.drawable.out_of_the_box).into(holder.bookCover);
+                else if(img_url == "five_point")
+                    Picasso.with(getContext()).load(R.drawable.five_point_someone).into(holder.bookCover);
+                else if(img_url == "one_night")
+                    Picasso.with(getContext()).load(R.drawable.one_night).into(holder.bookCover);
+                else if(img_url == "revolution")
+                    Picasso.with(getContext()).load(R.drawable.revolution2020).into(holder.bookCover);
+                else if(img_url == "half_girlfriend")
+                    Picasso.with(getContext()).load(R.drawable.half_girlfriend).into(holder.bookCover);
+                else if(img_url == "two_states")
+                    Picasso.with(getContext()).load(R.drawable.two_states).into(holder.bookCover);
+                else if(img_url == "3_mistakes")
+                    Picasso.with(getContext()).load(R.drawable.three_mistakes).into(holder.bookCover);
+
+            }
             //holder.bookCover.setImageResource(books.get(position).book_cover_id);
         }
 
